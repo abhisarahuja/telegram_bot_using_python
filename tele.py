ec5917ff42193f633f853f87bbf9634f9e4e3fbf
Mokidada <142949824+Mokidada@users.noreply.github.com>
2023-08-25 01:14:55 +0530

    Update tele.py

--- tele.py
+++ tele.py
@@ -1,9 +1,9 @@
 from telegram import *
 from telegram.ext import *
 
-bot = Bot("Paste your Telegram bot token here")
+bot = Bot("6433016171:AAEWFcSsjfe_TOmjWg4KeReV9xfYsg4Kfr8")
 #print(bot.get_me())
-updater = Updater("Paste your Telegram bot token here", use_context=True)
+updater = Updater("6433016171:AAEWFcSsjfe_TOmjWg4KeReV9xfYsg4Kfr8", use_context=True)
 
 dispatcher= updater.dispatcher
 
